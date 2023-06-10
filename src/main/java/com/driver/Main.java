5636de0c146d6754f404fb5fb34af4bdde0cc2f5
Niteshkarmore7888 <niteshkarmo444@gmail.com>
2023-06-10 18:04:08 +0530

    Adding Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly rwonly =new RWOnly();
+
+        // Expected 0 arguments but found 1
+
+
+        rwonly.setName("Nitesh");
+        rwonly.getName();
+    }
   
 }
